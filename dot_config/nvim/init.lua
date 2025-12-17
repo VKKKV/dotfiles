55dc38bb798f65b5a1403dc14774120ee96f5636
vkkkv <vkkkv@example.com>
2025-12-17 20:24:29 +0800

    i suck at vibe coding

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -22,124 +22,58 @@ require("lazy").setup({
         "nvim-treesitter/nvim-treesitter",
         lazy = false,
         build = ":TSUpdate",
-        config = function()
-            require("nvim-treesitter").setup({
-                ensure_installed = { "bash", "c", "javascript", "lua", "markdown", "python", "rust", "typescript", "typst", "vim", "vue", "glsl", },
-                sync_install = false,
-                auto_install = true,
-                indent = { enable = true },
-                highlight = {
-                    enable = true,
-                    disable = function(lang, buf)
-                        if lang == "html" then
-                            print("disabled treesitter for html")
-                            return true
-                        end
-                        local max_filesize = 233 * 1024 -- 233 KB
-                        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                        if ok and stats and stats.size > max_filesize then
-                            vim.notify(
-                                "File larger than 100KB treesitter disabled for performance",
-                                vim.log.levels.WARN,
-                                { title = "Treesitter" }
-                            )
-                            return true
-                        end
-                    end,
-                },
-            })
-        end,
+        opts = {
+            ensure_installed = { "bash", "c", "javascript", "lua", "markdown", "python", "rust", "typescript", "typst", "vim", "vue", "glsl", },
+            sync_install = false,
+            auto_install = true,
+            indent = { enable = true },
+            highlight = {
+                enable = true,
+                disable = function(lang, buf)
+                    if lang == "html" then
+                        print("disabled treesitter for html")
+                        return true
+                    end
+                    local max_filesize = 233 * 1024 -- 233 KB
+                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                    if ok and stats and stats.size > max_filesize then
+                        vim.notify(
+                            "File larger than 100KB treesitter disabled for performance",
+                            vim.log.levels.WARN,
+                            { title = "Treesitter" }
+                        )
+                        return true
+                    end
+                end,
+            },
+        },
     },
     -- UI: Status Line
     {
         "nvim-lualine/lualine.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            require("lualine").setup({
-                options = { theme = "gruvbox" },
-                tabline = {
-                    lualine_a = { "buffers" },
-                    lualine_b = { "branch" },
-                    lualine_c = { "filename" },
-                    lualine_x = {},
-                    lualine_y = {},
-                    lualine_z = { "tabs" },
-                },
-            })
-        end,
+        opts = {
+            options = { theme = "gruvbox" },
+            tabline = {
+                lualine_a = { "buffers" },
+                lualine_b = { "branch" },
+                lualine_c = { "filename" },
+                lualine_x = {},
+                lualine_y = {},
+                lualine_z = { "tabs" },
+            },
+        },
     },
     -- UI: File Explorer
     {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "MunifTanjim/nui.nvim",
-            "nvim-tree/nvim-web-devicons",
+        "stevearc/oil.nvim",
+        opts = {
+            delete_to_trash = true,
+            view_options = { show_hidden = true, },
         },
-        keys = { { "<leader>e", ":Neotree toggle<CR>" } },
-        config = function()
-            require("neo-tree").setup({
-                filesystem = {
-                    commands = {
-                        avante_add_files = function(state)
-                            local node = state.tree:get_node()
-                            local filepath = node:get_id()
-                            local relative_path = require("avante.utils").relative_path(filepath)
-
-                            local sidebar = require("avante").get()
-
-                            local open = sidebar:is_open()
-                            -- 确保 avante 侧边栏已打开
-                            if not open then
-                                require("avante.api").ask()
-                                sidebar = require("avante").get()
-                            end
-
-                            sidebar.file_selector:add_selected_file(relative_path)
-
-                            -- 删除 neo tree 缓冲区
-                            if not open then
-                                sidebar.file_selector:remove_selected_file("neo-tree filesystem [1]")
-                            end
-                        end,
-                    },
-                    window = {
-                        mappings = {
-                            ["oa"] = "avante_add_files",
-                        },
-                    },
-                },
-            })
-        end,
-    },
-    {
-        "antosha417/nvim-lsp-file-operations",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-neo-tree/neo-tree.nvim", -- makes sure that this loads after Neo-tree.
-        },
-        config = function()
-            require("lsp-file-operations").setup()
-        end,
-    },
-    {
-        "s1n7ax/nvim-window-picker",
-        version = "2.*",
-        config = function()
-            require("window-picker").setup({
-                filter_rules = {
-                    include_current_win = false,
-                    autoselect_one = true,
-                    -- filter using buffer options
-                    bo = {
-                        -- if the file type is one of following, the window will be ignored
-                        filetype = { "neo-tree", "neo-tree-popup", "notify" },
-                        -- if the buffer type is one of following, the window will be ignored
-                        buftype = { "terminal", "quickfix" },
-                    },
-                },
-            })
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function() require("oil").setup({})
+            vim.keymap.set({ "n", "x" }, "<leader>e", "<CMD>Oil<CR>", { silent = true })
         end,
     },
     -- UI: Fuzzy Finder
@@ -154,52 +88,29 @@ require("lazy").setup({
         },
     },
     -- UI: Indent Guides
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        main = "ibl",
-        opts = {},
-    },
+    { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
     -- EDITOR: Commenting
-    {
-        "numToStr/Comment.nvim",
-        opts = {},
-        lazy = false,
-    },
+    { "numToStr/Comment.nvim", lazy = false, opts = {} },
     -- EDITOR: Surround
-    {
-        "kylechui/nvim-surround",
-        event = "VeryLazy",
-        config = function()
-            require("nvim-surround").setup({})
-        end,
-    },
+    { "kylechui/nvim-surround", event = "VeryLazy" },
     -- EDITOR: Multi Cursor
     -- default keymaps:
     -- Ctrl-n to select word and add cursorcolumn
     -- Ctrl-Down / Ctrl-Up to add cursor below/above
     { "mg979/vim-visual-multi", branch = "master" },
     -- EDITOR: Auto Pairs
-    {
-        "windwp/nvim-autopairs",
-        event = "InsertEnter",
-        opts = {},
-    },
+    { "windwp/nvim-autopairs", event = "InsertEnter" },
     -- THEME: Gruvbox
-    {
-        "ellisonleao/gruvbox.nvim",
-        priority = 1000,
-        config = function()
-            vim.cmd.colorscheme("gruvbox")
-        end,
-    },
+    { "ellisonleao/gruvbox.nvim", priority = 1000, config = function() vim.cmd.colorscheme("gruvbox") end},
     -- SNIPPETS: LuaSnip
     {
         "L3MON4D3/LuaSnip",
         version = "v2.*",
+        dependencies = { "rafamadriz/friendly-snippets" },
         build = "make install_jsregexp",
-
         config = function()
             require("luasnip").setup({ enable_autosnippets = true })
+            require("luasnip.loaders.from_vscode").lazy_load()
             require("luasnip.loaders.from_lua").load({ paths = "./snippets" })
         end,
     },
@@ -328,21 +239,9 @@ require("lazy").setup({
         dependencies = { "nvim-tree/nvim-web-devicons" },
         cmd = "Trouble",
         keys = {
-            {
-                "<leader>lq",
-                "<cmd>Trouble diagnostics toggle<cr>",
-                desc = "Diagnostics (Trouble)",
-            },
-            {
-                "<leader>ls",
-                "<cmd>Trouble symbols toggle focus=false<cr>",
-                desc = "Symbols (Trouble)",
-            },
-            {
-                "<leader>la",
-                "<cmd>Trouble qflist toggle<cr>",
-                desc = "Quickfix List (Trouble)",
-            },
+            { "<leader>ld", "<cmd>Trouble diagnostics toggle<cr>", },
+            { "<leader>ls", "<cmd>Trouble symbols toggle focus=false<cr>", },
+            { "<leader>la", "<cmd>Trouble qflist toggle<cr>", },
         },
         config = function()
             require("trouble").setup({
@@ -367,94 +266,91 @@ require("lazy").setup({
     {
         "chomosuke/typst-preview.nvim",
         ft = "typst",
-        build = function() require("typst-preview").update() end,
+        build = function()
+            require("typst-preview").update()
+        end,
+    },
+    -- AI TOOLS
+    {
+        "qapquiz/sidekick.nvim",
+        opts = { cli = { mux = { backend = "tmux", enabled = true, }, }, },
+        keys = {
+            {
+                "<leader>ao",
+                function()
+                    require("sidekick.cli").toggle({ name = "opencode", focus = true })
+                end,
+                desc = "Sidekick Toggle opencode",
+            },
+            {
+                "<leader>as",
+                function()
+                    require("sidekick.cli").select()
+                end,
+                desc = "Select CLI",
+            },
+            {
+                "<leader>ad",
+                function()
+                    require("sidekick.cli").close()
+                end,
+                desc = "Detach a CLI Session",
+            },
+            {
+                "<leader>at",
+                function()
+                    require("sidekick.cli").send({ msg = "{this}" })
+                end,
+                mode = { "x", "n" },
+                desc = "Send This",
+            },
+            {
+                "<leader>af",
+                function()
+                    require("sidekick.cli").send({ msg = "{file}" })
+                end,
+                desc = "Send File",
+            },
+            {
+                "<leader>av",
+                function()
+                    require("sidekick.cli").send({ msg = "{selection}" })
+                end,
+                mode = { "x" },
+                desc = "Send Visual Selection",
+            },
+            {
+                "<leader>ap",
+                function()
+                    require("sidekick.cli").prompt()
+                end,
+                mode = { "n", "x" },
+                desc = "Sidekick Select Prompt",
+            },
+        },
     },
     -- COPILOT
     {
         "zbirenbaum/copilot.lua",
         cmd = "Copilot",
         event = "InsertEnter",
-        config = function()
-            require("copilot").setup({
-                suggestion = {
-                    enabled = true,
-                    auto_trigger = true,
-                    -- keymap to accept suggestion
-                    keymap = {
-                        accept = "<C-A>",
-                    },
-                },
-                panel = { enabled = false },
-                filetypes = { markdown = false, help = false },
-            })
-        end,
-    },
-    -- AI
-    {
-        "yetone/avante.nvim",
-        build = "make",
-        event = "VeryLazy",
-        version = false,
-        ---@module 'avante'
-        ---@type avante.Config
         opts = {
-            provider = "siliconflow",
-            providers = {
-                claude = {
-                    endpoint = "https://api.anthropic.com",
-                    model = "claude-sonnet-4-20250514",
-                    timeout = 30000,
-                    extra_request_body = {
-                        temperature = 0.75,
-                        max_tokens = 20480,
-                    },
-                },
-                siliconflow = {
-                    __inherited_from = "openai",
-                    endpoint = "https://api.siliconflow.cn/v1",
-                    api_key_name = "SILICONFLOW_API_KEY",
-                    model = "MiniMaxAI/MiniMax-M2",
-                },
-                openrouter = {
-                    __inherited_from = "openai",
-                    endpoint = "https://openrouter.ai/api/v1",
-                    api_key_name = "OPENROUTER_API_KEY",
-                    model = "deepseek/deepseek-r1",
+            suggestion = {
+                enabled = true,
+                auto_trigger = true,
+                -- keymap to accept suggestion
+                keymap = {
+                    accept = "<C-a>",
                 },
             },
-        },
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "MunifTanjim/nui.nvim",
-            --- 以下依赖项是可选的，
-            "echasnovski/mini.pick", -- 用于文件选择器提供者 mini.pick
-            "nvim-telescope/telescope.nvim", -- 用于文件选择器提供者 telescope
-            "hrsh7th/nvim-cmp", -- avante 命令和提及的自动完成
-            "ibhagwan/fzf-lua", -- 用于文件选择器提供者 fzf
-            "nvim-tree/nvim-web-devicons", -- 或 echasnovski/mini.icons
-            "zbirenbaum/copilot.lua", -- 用于 providers='copilot'
-            {
-                -- 支持图像粘贴
-                "HakonHarnes/img-clip.nvim",
-                event = "VeryLazy",
-                opts = {
-                    -- 推荐设置
-                    default = {
-                        embed_image_as_base64 = false,
-                        prompt_for_file_name = false,
-                        drag_and_drop = {
-                            insert_mode = true,
-                        },
-                    },
-                },
-            },
+            panel = { enabled = false },
+            filetypes = { markdown = false, help = false },
         },
     },
 })
 
-local opt = vim.opt
-
 -- Settings
+local opt = vim.opt
 opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20" -- Keymaps
 opt.number = true
 opt.relativenumber = true
@@ -490,7 +386,6 @@ opt.foldopen = "mark,percent,quickfix,search,tag,undo"
 
 -- Keymaps
 local keymap = vim.keymap.set
-
 keymap("n", "<leader>w", "<CMD>write<CR>", { silent = true })
 keymap("n", "<leader>q", ":q<CR>")
 keymap("n", "<leader>x", ":bd<CR>")
@@ -510,54 +405,36 @@ keymap("n", "<leader>j", ":vertical resize +2<CR>")
 
 -- Comment
 -- Use Ctrl+/ to toggle comments in normal and visual mode
-keymap("n", "<C-/>", function() vim.api.nvim_feedkeys("gcc", "x", true) end, { desc = "Toggle Line Comment" })
-
-keymap("v", "<C-/>", function() vim.api.nvim_feedkeys("gb", "v", true) end, { desc = "Toggle Line Comment" })
+keymap("n", "<C-_>", function() vim.api.nvim_feedkeys("gcc", "x", true) end, { desc = "Toggle Line Comment" })
+keymap("v", "<C-_>", function() vim.api.nvim_feedkeys("gb", "v", true) end, { desc = "Toggle Line Comment" })
 
 -- Auto Commands
 local autocmd = vim.api.nvim_create_autocmd
-
-autocmd("BufWritePre", {
-    pattern = "*",
-    command = ":%s/\\s\\+$//e",
-})
-
+autocmd("BufWritePre", { pattern = "*", command = ":%s/\\s\\+$//e", })
 autocmd("FileType", {
   pattern = "typst",
   callback = function()
     vim.keymap.set("n", "<leader>o", ":TypstPreviewToggle<CR>", { noremap = true, silent = true, })
   end,
 })
-
 autocmd("FileType", {
   pattern = "markdown",
   callback = function()
     vim.keymap.set("n", "<leader>o", ":MarkdownPreviewToggle<CR>", { noremap = true, silent = true, })
   end,
 })
-
 autocmd("LspAttach", {
     callback = function(e)
         local opts = { buffer = e.buf }
+        vim.keymap.set("n", "<leader>la", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>lr", function() vim.lsp.buf.rename() end, opts)
         vim.keymap.set("n", "<leader>d", function() vim.lsp.buf.definition() end, opts)
         vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-
-        -- format key
-        -- vim.keymap.set("n", "<leader>=", vim.lsp.buf.format)
-
-        -- Use conform.nvim for formatting
         vim.keymap.set({ "n", "v" }, "<leader>=", function()
             require("conform").format({
                 async = true,
                 lsp_fallback = true, -- Fallback to LSP formatting if no CLI formatter is found
             })
         end, { desc = "Format code" })
-
-        vim.keymap.set("n", "<leader>la", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("n", "<leader>lr", function() vim.lsp.buf.rename() end, opts)
-
-        -- vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-        -- vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-        -- vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
     end,
 })
